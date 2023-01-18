ffbc0bf94537c5e14de4c2422742460e798b5514
Garrett Kelly <garrettk@subpop.com>
2023-01-18 11:40:58 -0800

    AUdio and words big

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let osc;
+let tremelo;
+
 let myDictionary;
 // preload JSON data
 function preload(){
@@ -5,32 +8,37 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(2000, 1500);
   background(0);
   textSize(32);
   textAlign(CENTER);
   fill(255);
+
+  osc = new p5.Oscillator();
+  osc.freq(220);
+  osc.setType('sine');
+  osc.amp(0.5);
 }
 
 function draw() {
   let keys = Object.keys(myDictionary);
     let gray = random(255);
     fill(gray);
-    let angle = random(-10, 10);
-    push();
-    translate(random(width), random(height));
-    rotate(angle);
-  
-  for (var i = 0; i < 50; i++) {
+
+  for (var i = 0; i < 100; i++) {
       textSize(i);
 
     let randomKey = keys[int(random(keys.length))];
     text(randomKey, random(width), random(height));
+
+    let freq = random(200, 800);
+    let amp = random(0.1, 0.5);
+
+    osc.freq(freq);
+    osc.amp(amp);
   }
 }
 
-function setup() {
-  createCanvas(800, 600);
+function mousePressed() {
+  osc.start();
 }
-
-
